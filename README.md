20033114a3d52df6060f288fb0a5900f4e97ebee
aparnagk76 <aparnagk76@gmail.com>
2020-11-22 20:29:53 -0800

    use modules to setup acme

--- README.md
+++ README.md
@@ -8,16 +8,18 @@
 
 ## Providers
 
-| Name | Version |
-|------|---------|
-| aws | ~> 3.0 |
+No provider.
 
 ## Inputs
 
 | Name | Description | Type | Default | Required |
 |------|-------------|------|---------|:--------:|
-| key\_name | the name of a key | `string` | `"aparna1"` | no |
-| private\_key\_path | the path of a private key | `string` | `"~/.ssh/aparna1.pem"` | no |
+| backend\_instance\_count | the count of backend ec2 instances | `number` | `3` | no |
+| egress\_cidr\_blocks | list of cidr blocks to allow outgoing traffic | `list(string)` | <pre>[<br>  "0.0.0.0/0"<br>]</pre> | no |
+| environment\_name | the name of an environment | `string` | `"acme-dev"` | no |
+| ingress\_cidr\_blocks | list of cidr blocks to allow incoming traffic | `list(string)` | <pre>[<br>  "67.169.175.5/32",<br>  "10.2.1.0/24"<br>]</pre> | no |
+| instance\_type | the type of ec2 instance | `string` | `"t2.micro"` | no |
+| vpc\_cidr\_block | vpc cidr\_block | `string` | `"10.2.0.0/16"` | no |
 
 ## Outputs
 
--- variables.tf
+++ variables.tf
@@ -1,11 +1,35 @@
-variable key_name {
+variable vpc_cidr_block {
   type        = string
-  description = "the name of a key"
-  default     = "aparna1"
+  description = "vpc cidr_block"
+  default     = "10.2.0.0/16"
 }
 
-variable "private_key_path" {
+variable environment_name {
   type        = string
-  description = "the path of a private key"
-  default     = "~/.ssh/aparna1.pem"
+  description = "the name of an environment"
+  default     = "acme-dev"
+}
+
+variable ingress_cidr_blocks {
+  type        = list(string)
+  description = "list of cidr blocks to allow incoming traffic"
+  default     = ["67.169.175.5/32", "10.2.1.0/24"]
+}
+
+variable egress_cidr_blocks {
+  type        = list(string)
+  description = "list of cidr blocks to allow outgoing traffic"
+  default     = ["0.0.0.0/0"]
+}
+
+variable instance_type {
+  type        = string
+  description = "the type of ec2 instance"
+  default     = "t2.micro"
+}
+
+variable backend_instance_count {
+  type        = number
+  description = "the count of backend ec2 instances"
+  default     = 3
 }
